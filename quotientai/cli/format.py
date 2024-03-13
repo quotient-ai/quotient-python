41f09366a8db2b01409a09876ebe88af2fe3314f
Vic Weiss <vicweiss@MacBook-Pro-4.local>
2024-03-13 14:54:56 -0400

    Print Full File Path

--- quotientai/cli/format.py
+++ quotientai/cli/format.py
@@ -3,6 +3,9 @@ import textwrap
 from prettytable import PrettyTable
 import pandas as pd
 import json
+import os
+
+
 def format_api_keys_table(data):
     table = PrettyTable()
     table.field_names = ["Name", "Revoked", "Created At", "Expires At"]
@@ -224,6 +227,7 @@ def format_tasks_table(data):
 
     return table
 
+
 def format_results_summary_table(data):
     table = PrettyTable()
     table.field_names = [
@@ -282,6 +286,7 @@ def format_results_table(data):
 
     return table, has_more_results
 
+
 def save_results_to_file(data):
     data_to_frame = []
     for item in data["results"]:
@@ -292,14 +297,16 @@ def save_results_to_file(data):
             "answer": content["answer"],
             "completion": content["completion"],
             "context": content["context"],
-            "formatted_content": content["formatted_content"]
+            "formatted_content": content["formatted_content"],
         }
         data_to_frame.append(row)
 
     df = pd.DataFrame(data_to_frame)
     file_name = f"quotient-results-{data['id']}.csv"
     df.to_csv(file_name, index=False)
-    print(f"Results saved to {file_name}")
+    full_path = os.path.abspath(file_name)
+    print(f"Results saved to {full_path}")
+
 
 def save_metrics_to_file(data):
     df = pd.json_normalize(data, "results")
@@ -308,7 +315,9 @@ def save_metrics_to_file(data):
     description = df.describe()
     file_name = f"quotient-metrics-{data['id']}.csv"
     description.to_csv(file_name, index=False)
-    print(f"Metrics saved to {file_name}")
+    full_path = os.path.abspath(file_name)
+    print(f"Metrics saved to {full_path}")
+
 
 def save_eval_metadata_to_file(data):
     selected_info = {
@@ -317,10 +326,11 @@ def save_eval_metadata_to_file(data):
         "completed_at": data.get("completed_at"),
         "id": data.get("id"),
         "task_type": data.get("task_type"),
-        "seed": data.get("seed")
+        "seed": data.get("seed"),
     }
 
     file_name = f"quotient-eval-metadata-{data['id']}.json"
-    with open(file_name, 'w') as json_file:
+    with open(file_name, "w") as json_file:
         json.dump(selected_info, json_file, indent=4)
-    print(f"Evaluation metadata saved to {file_name}")
+    full_path = os.path.abspath(file_name)
+    print(f"Evaluation metadata saved to {full_path}")
