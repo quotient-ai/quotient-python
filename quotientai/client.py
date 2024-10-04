9441845e30266b0dd0ed2b43f7c1aff9b388460f
Freddie Vargus <freddie@quotientai.co>
2024-10-04 15:11:23 -0400

    Formatting

--- quotientai/client.py
+++ quotientai/client.py
@@ -35,7 +35,9 @@ class QuotientClient:
         # Base URL for the Supabase project
         self.supabase_url = "https://hhqppcqltklzfpggdocb.supabase.co"
 
-        self.eval_scheduler_url = "http://eval-scheduler-alb-887401167.us-east-2.elb.amazonaws.com"
+        self.eval_scheduler_url = (
+            "http://eval-scheduler-alb-887401167.us-east-2.elb.amazonaws.com"
+        )
 
         self.supaclient = SyncPostgrestClient(
             self.supabase_url + "/rest/v1", headers={"apiKey": self.public_api_key}
