b2e7ab889583c7ffdcbdec340ab24a47d6dab89a
Freddie Vargus <freddie@quotientai.co>
2025-06-11 14:25:21 -0400

    Update default tracing endpoint

--- quotientai/_constants.py
+++ quotientai/_constants.py
@@ -1,2 +1,2 @@
 TRACER_NAME = "quotient.sdk.python"
-DEFAULT_TRACING_ENDPOINT = "http://localhost:8082/api/v1/traces" #"https://api.quotientai.co/api/v1/traces"
+DEFAULT_TRACING_ENDPOINT = "https://api.quotientai.co/api/v1/traces"
