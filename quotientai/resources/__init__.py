84ecc8cd9944719e0f443cb4c91ae6aa7874282c
Freddie Vargus <freddie@quotientai.co>
2025-06-20 13:45:40 -0400

    Move imports

--- quotientai/resources/__init__.py
+++ quotientai/resources/__init__.py
@@ -1,8 +1,10 @@
+from quotientai.resources.auth import AuthResource
 from quotientai.resources.logs import LogsResource
 from quotientai.resources.logs import AsyncLogsResource
 from quotientai.tracing.core import TracingResource
 
 __all__ = [
+    "AuthResource",
     "LogsResource",
     "AsyncLogsResource",
     "TracingResource",
