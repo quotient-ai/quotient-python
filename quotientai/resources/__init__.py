5ba6c314cc521a0bff5ac57456c012e88a1d0a6f
Freddie Vargus <freddie@quotientai.co>
2025-08-07 15:08:25 -0400

    Add traces resource for list and get

--- quotientai/resources/__init__.py
+++ quotientai/resources/__init__.py
@@ -1,11 +1,13 @@
 from quotientai.resources.auth import AuthResource
 from quotientai.resources.logs import LogsResource
 from quotientai.resources.logs import AsyncLogsResource
+from quotientai.resources.tracing import TracesResource
 from quotientai.tracing.core import TracingResource
 
 __all__ = [
     "AuthResource",
     "LogsResource",
     "AsyncLogsResource",
+    "TracesResource",
     "TracingResource",
 ]
